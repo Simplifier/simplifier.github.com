2988d874211f29ce87c32da336f6ec11cc393396
Simplifier <iseldomused@gmail.com>
2013-02-19 17:54:51 +0400

    added upload.php

---
+++ virtualPlasty/upload.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+$filename = $_FILES['image']['name']; //имя файла
+$filetmpname = $_FILES['image']['tmp_name'];	//адрес tmp
+$fileType = $_FILES["image"]["type"]; //тип файла
+$fileSizekB = ($_FILES["image"]["size"] / 1024 ); //размер файла
+
+echo 'temp name: '.$filetmpname."\nname: ".$filename."\nsize: ".$fileSizekB.' kB';
+?>
